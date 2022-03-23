2fe2617175786cb377a3818ffb35085a3dcaea3b
Rodrigo Coimbra <rodscoimbra@gmail.com>
2022-03-23 23:05:09 +0000

    Add files via upload

--- FIRE.c
+++ FIRE.c
@@ -6,22 +6,20 @@ void main()
     int tam, i, k = 1;
     char *s;
     scanf(" %d", &tam);
+    if (tam == 0)
+    {
+        return;
+    }
     if ((s = (char *)malloc(sizeof(char) * (tam + 1))) == NULL)
     {
         return;
     }
-    if (tam == 0)
-    {
-        free(s);
-        return;
-    }
     scanf(" %s", s);
-
     for (i = 1; i != tam; i++, k = 1)
     {
         while (i - k >= 0 && s[i - k] == '0')
         {
-            k++;
+            k += 2;
         }
         if (i - k < 0)
         {
@@ -57,10 +55,10 @@ void main()
                 s[i - k] = '0';
             }
             break;
-        default:
-            free(s);
-            return;
-            break;
+            /*default:
+                printf("DEI ERRO, %d - %d - %c - %d\n\n\n", i, tam, s[i], k);
+                return;
+                break;*/
         }
     }
     for (i = 0; i != tam; i++)
